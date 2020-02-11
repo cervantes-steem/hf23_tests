9c42fc29e64bad0889ad70cd9a708d968c81b8da
pgarciagon <pgarciagon@gmail.com>
2020-02-11 17:37:13 +0100

    Testing

--- test_smt_create.js
+++ test_smt_create.js
@@ -1,4 +1,5 @@
 
+const moment =  require("moment");
 var steem = require('steem');
 require('dotenv').config();
 steem.api.setOptions({url: 'https://testnet.steemitdev.com/', useAppbaseApi :  true, address_prefix : 'TST', 'chain_id' : "98b69bd7cbe4d27864e2f06d9f74ba50aed0b7f511436ef9c7b80f36f3ff5859"});
@@ -254,6 +255,160 @@ async function bulk_smt_object_create() {
 
   await broadcast(tx, s_active_key_0);
 
+
+  operations = [];
+  for (let i = 0; i < smts.length; i++) {
+
+      // Generate random number of emissions
+      let schedule_time = moment();
+
+      let emission = Math.floor(Math.random() * 21659);
+
+      // All the emissions will happen in the next 1 days
+      const hours_to_add = Math.floor(Math.random() * 24);
+      schedule_time.add('hours', hours_to_add);
+      let schedule_time_str = schedule_time.format("YYYY-MM-DDTHH:mm:ss");
+
+      schedule_time.add('hours',  Math.floor(Math.random() * 720))
+      let rep_time_str = schedule_time.format("YYYY-MM-DDTHH:mm:ss")
+
+      let token_units = [];
+
+      while (token_units.length == 0) {
+          if (Math.random() > 0.5)
+              token_units.push(['$market_maker', Math.floor(Math.random() * 65534) + 1]);
+          if (Math.random() > 0.5)
+              token_units.push(['$rewards', Math.floor(Math.random() * 65534) + 1]);
+          if (Math.random() > 0.5)
+              token_units.push(['$vesting', Math.floor(Math.random() * 65534) + 1]);
+          if (Math.random() > 0.5)
+              token_units.push(['cervantes', Math.floor(Math.random() * 65534) + 1]);
+          if (Math.random() > 0.5)
+              token_units.push(['domenico', Math.floor(Math.random() * 65534) + 1]);
+      }
+
+      operations.push([
+              'smt_setup_emissions', {
+              'control_account': s_username_0,
+              'symbol': {'nai': smts[i].nai, 'precision': smts[i].precision},
+              'schedule_time': schedule_time_str,
+              'emissions_unit': {
+                  'token_unit': token_units
+              },
+              'interval_seconds': (emission < 21600 ? 21600 : emission),
+              'emission_count': Math.floor(Math.random() * 21659),
+              'lep_time': schedule_time_str,
+              'lep_abs_amount': Math.floor(Math.random() *  4294967295),
+              'lep_rel_amount_numerator': 1,
+              'rep_time': rep_time_str,
+              'rep_abs_amount': Math.floor(Math.random() * 4294967295),
+              'rep_rel_amount_numerator': 0,
+              'rel_amount_denom_bits': 0,
+              'remove': false,
+              'floor_emissions': Math.random() > 0.5,
+              'extensions': []
+              }]
+      )
+  }
+
+  tx = {
+      'operations': operations
+  };
+
+  let result = await broadcast(tx, s_active_key_0);
+  if(!result.noError) {
+    console.log(result);
+  }
+
+
+}
+
+
+async function create_smt_full_setup() {
+
+  let nai_pool = await steem.api.callAsync('database_api.get_nai_pool', {});
+  const nai = nai_pool.nai_pool[0].nai;
+
+  let tx = {
+      'operations': [[
+          'smt_create', {
+              'control_account': s_username_0,
+              'symbol': {'nai': nai, 'precision': 3},
+              'smt_creation_fee': {'amount': '1000', 'precision': 3, 'nai': '@@000000013'},
+              'precision': 3,
+          }]]
+  };
+
+  console.log("Broadcasting smt_create op...")
+  await broadcast(tx, s_active_key_0);
+
+
+  let schedule_time = moment();
+
+  // Make sure it launches in the next 10 seconds
+  // I have a one hour offset compared to blockchain time, you might want to remove this
+  schedule_time.subtract(59, 'minute');
+  schedule_time.subtract(50, 'second');
+
+  let schedule_time_str = schedule_time.format("YYYY-MM-DDTHH:mm:ss");
+
+  tx = {
+      'operations': [[
+          'smt_setup_emissions', {
+              'control_account': s_username_0,
+              'symbol': {'nai': nai, 'precision': 3},
+              'schedule_time': schedule_time_str,
+              'emissions_unit': {
+                  'token_unit': [
+                      ['$market_maker', 1],
+                      ['$rewards', 1],
+                      ['$vesting', 1],
+                  ],
+              },
+              'interval_seconds': 21600,
+              'emission_count':  10000,
+              'lep_time' : '1970-01-01T00:00:00',
+              'rep_time' : '1970-01-01T00:00:00',
+              'lep_abs_amount' : 0,
+              'rep_abs_amount': 0,
+              'lep_rel_amount_numerator' : 1,
+              'rep_rel_amount_numerator' : 0,
+              'rel_amount_denom_bits' : 0,
+              'remove' : false,
+              'floor_emissions' : false,
+          }]]
+  };
+
+  console.log("Broadcasting smt_setup_emissions op...")
+  let result = await broadcast(tx, s_active_key_0);
+  if(!result.noError) {
+    console.log(result);
+  }
+
+
+
+  tx = {
+      'operations': [[
+          'smt_setup', {
+              'control_account': s_username_0,
+              'symbol': {'nai': nai, 'precision': 3},
+              'max_supply': 60000000,
+              'contribution_begin_time': schedule_time_str,
+              'contribution_end_time': schedule_time_str,
+              'launch_time': schedule_time_str,
+              'steem_units_min': 0,
+              'min_unit_ratio': 0,
+              'max_unit_ratio': 0,
+              'extensions': []
+          }]]
+  };
+
+  console.log("Broadcasting smt_setup op...")
+  await broadcast(tx, s_active_key_0);
+
+
+  console.log(`created smt with nai : ${nai}`);
+
 }
 
 //let new_smt_nai = '@@979651727';
@@ -263,4 +418,5 @@ async function bulk_smt_object_create() {
 //test_transfer_tests();
 //test_transfer_test_back();
 //test_comment();
-bulk_smt_object_create();
+//bulk_smt_object_create();
+create_smt_full_setup();
